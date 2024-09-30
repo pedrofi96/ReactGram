d17c1fcdfb8b768f4eaa144717c15a1d1dfb28b1
Pedro Filipe Tavares Baptista <piedro123@hotmail.com>
2024-09-30 10:16:42 -0300

    igualando homeoffice ao escritorio

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose')
 
 //conexão com banco de dados mongodb
+//mongodb+srv://<db_username>:<db_password>@reactgramtest.m0yom.mongodb.net/?retryWrites=true&w=majority&appName=ReactGramTest
+//OzfRL9I0j1Fe7OVn
+//user: pedrofi
+
