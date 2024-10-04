31a29e5b4db7cd657e4d10a96ccb1ee4bb02c458
Pedro Filipe Tavares Baptista <pedro.baptista@rootssa.ba.gov.br>
2024-10-04 11:28:13 -0300

    pequenas modificações no frontend

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,29 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./Auth.css";
 
 //components
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import { useState, useEffect } from 'react'
-
-const handleSubmit = (e) =>{
-  e.preventDefault()
-}
+import { useState, useEffect } from "react";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+    console.log(user)
+  };
   return (
-    <div>
+    <div id="register">
       <h2>ReactGram</h2>
-      <p>Cadastre-se para acompanhar seus amigos</p>
-      <form onSubmit={handleSubmit}> 
-          <input type='text' placeholder='Nome'></input>
-          <input type='email' placeholder='E-Mail'></input>
-          <input type='password' placeholder='Senha'></input>
-          <input type='password' placeholder='Confirmar Senha'></input>
-          <input type='submit' value={'Cadastrar'}></input>
+      <p className="subtitle">Cadastre-se para acompanhar seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" onChange={(e)=> setName(e.target.value)}></input>
+        <input type="email" placeholder="E-Mail" onChange={(e)=> setEmail(e.target.value)}></input>
+        <input type="password" placeholder="Senha" onChange={(e)=>{setPassword(e.target.value)}}></input>
+        <input type="password" placeholder="Confirmar Senha" onChange={(e)=>{setConfirmPassword(e.target.value)}}></input>
+        <input type="submit" value={"Cadastrar"}></input>
       </form>
-      <p>Já tem conta? <Link to={'/login'}>Clique Aqui</Link> </p>
+      <p>
+        Já tem conta? <Link to={"/login"}>Clique Aqui</Link>{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
